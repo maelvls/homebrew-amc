d4615710dd32eb81cf85175757e26742500a9670
Maël Valais <mael.valais@gmail.com>
2018-03-13 17:21:34 +0100

    amc: update to 1.3.0-2186

--- auto-multiple-choice.rb
+++ auto-multiple-choice.rb
@@ -1,10 +1,10 @@
 class AutoMultipleChoice < Formula
   desc "Auto Multiple Choice (AMC) helps you prepare printed tests for your students and mark them using PDF scans"
   homepage "https://www.auto-multiple-choice.net"
-  url "https://bitbucket.org/auto-multiple-choice/auto-multiple-choice/downloads/auto-multiple-choice_1.3.0-2161-33b2895d5ed5_dist.tar.gz"
-  sha256 "e065972b0157a60a58f77f7753424b05b87656541e013e9461fb37c22f842d63"
-  version "1.3.0.2161"
-  revision 1
+  url "https://bitbucket.org/auto-multiple-choice/auto-multiple-choice/downloads/auto-multiple-choice_1.3.0-2186-7d6613da21d6_dist.tar.gz"
+  sha256 "0e660e8de4ceeff193d0b1f445aa66f2ed228ce5bd5e485fe4e8287223f3bfe9"
+  version "1.3.0.2186"
+  revision 0
   # I had to remove the 'head' as we cannot compile using latex in Homebrew.
   # Instead, we use the 'distributed' tarballs from the Bitbucket's Downloads
   # which already contain the doc and doc/sty. See (1) for details.
