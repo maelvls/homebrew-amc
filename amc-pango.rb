c241cab26a11564d808df05b9b6751559834895f
Maël Valais <mael@vls.dev>
2021-06-12 12:20:51 +0200

    force rebuild of amc-pango bottle

--- amc-pango.rb
+++ amc-pango.rb
@@ -6,7 +6,7 @@ class AmcPango < Formula
   homepage "https://www.pango.org/"
   url "https://download.gnome.org/sources/pango/1.42/pango-1.42.4.tar.xz"
   sha256 "1d2b74cd63e8bd41961f2f8d952355aa0f9be6002b52c8aa7699d9f5da597c9d"
-  revision 0
+  revision 1
 
   head do
     url "https://gitlab.gnome.org/GNOME/pango.git"
